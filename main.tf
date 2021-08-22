81cabf589a8f9897d14a13e9534fe0b407074250
Niall O'Riordan <nialloriordan@fundingcircle.com>
2021-08-22 13:48:12 +0100

    specify aws provider

--- main.tf
+++ main.tf
@@ -1,3 +1,13 @@
+terraform {
+  required_providers {
+    aws = {
+      source  = "hashicorp/aws"
+      version = "~> 3.55.0"
+    }
+  }
+
+  required_version = ">=1.0.4"
+}
 provider "aws" {
   region  = var.aws_region
   profile = var.aws_profile
